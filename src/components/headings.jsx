6f317e7f7671b4eb96a66e725c967e68e3f442e8
ZohaibAliJerov <zuhaib911alijerov@gmail.com>
2024-05-24 18:33:56 +0500

    add vector to headings

--- src/components/headings.jsx
+++ src/components/headings.jsx
@@ -5,7 +5,12 @@ const Headings = (props) => {
     <div className="flex flex-col items-center pt-14 pb-8">
       <StyledHeading className="text-3xl md:text-4xl lg:text-[48px] font-semibold text-center">
         {props.title}
-        <img src="./svgs/verctor.png" alt="" className="py-2" />
+        <img
+          src="./svgs/verctor.png"
+          alt=""
+          className="py-2"
+          style={{ width: "100%" }}
+        />
       </StyledHeading>
       <p className="text-center pt-10 " style={{ width: "70%" }}>
         {props.description}
